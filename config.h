957d932359bb27fa64634c846d146c6fc3ce5df1
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-07-25 18:47:47 -0300

    Use dmenu instead of rofi

--- config.h
+++ config.h
@@ -71,8 +71,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* [> component of dmenucmd, manipulated in spawn() <] */
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "rofi", "-show", "run", "-modi", "run,drun", "-theme", "dmenu", "-font", "Ubuntu Mono 10" };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "rofi", "-show", "run", "-modi", "run,drun", "-theme", "dmenu", "-font", "Ubuntu Mono 10" };
 static const char *sshcmd[] = { "rofi", "-show", "ssh", "-modi", "ssh", "-theme", "dmenu", "-font", "Ubuntu Mono 10" };
 static const char *vnccmd[] = { "rofi", "-show", "vnc", "-modi", "vnc:/home/rafael/.config/rofi/scripts/vnc.sh", "-theme", "dmenu", "-font", "Ubuntu Mono 10" };
 static const char *termcmd[] = { "alacritty", NULL };
