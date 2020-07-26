7e07c68938d77e8692977bd39e2726b06b6320cf
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-07-26 15:12:22 -0300

    Apply stacker patch

--- config.h
+++ config.h
@@ -76,6 +76,14 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+#define STACKKEYS(MOD,ACTION) \
+	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
+	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
+	{ MOD, XK_g,     ACTION##stack, {.i = 0 } }, \
+	//{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
+	//{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
+	//{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
+	//{ MOD, XK_q,     ACTION##stack, {.i = 0 } },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -87,11 +95,11 @@ static const char *termcmd[] = { "/bin/sh", "-c", "$TERMINAL", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	STACKKEYS(MODKEY,                          focus)
+	STACKKEYS(MODKEY|ShiftMask,                push)
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_Escape, spawn,          SHCMD("slock") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_equal,  incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_minus,  incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
