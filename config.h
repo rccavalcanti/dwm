ce4da6a7b986d91cbeb3132c4270712cff8c109d
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-09-12 20:52:31 -0300

    Add mouse binding for pavucontrol

--- config.h
+++ config.h
@@ -226,6 +226,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button1,        spawn,          SHCMD("gnome-calendar") },
+	{ ClkStatusText,        0,              Button2,        spawn,          SHCMD("pavucontrol") },
 	{ ClkStatusText,        0,              Button9,        spawn,          {.v = player_next } },
 	{ ClkStatusText,        0,              Button8,        spawn,          {.v = player_prev } },
 	{ ClkStatusText,        0,              Button3,        spawn,          {.v = player_pause } },
