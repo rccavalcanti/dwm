4c56e0af3d081dccaf35f7024e4b29b10b9a350f
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-09-13 22:57:00 -0300

    Add fallback colors when pywal is unavailable

--- config.h
+++ config.h
@@ -31,6 +31,18 @@ static const char *fonts[]          = { "Ubuntu Mono:size=10" };
 
 /* colorscheme from pywal */
 #include "/home/rafael/.cache/wal/colors-wal-dwm.h"
+/* use this if pywal unavailable
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	\/*               fg         bg         border   *\/
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+*/
 
 /* tagging */
 static const char *tags[] = { " WWW ", " SYS ", " DEV ", " DOC ", " VMs ", " MSG ", " MUS ", " NOT ", " ORG " };
