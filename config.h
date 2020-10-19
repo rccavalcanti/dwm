c612270498cabc54b52e66dcb98550a9cf3710da
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-10-19 00:33:37 -0300

    Move bar to top

--- config.h
+++ config.h
@@ -25,7 +25,7 @@ static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Ubuntu Mono:size=10" };
 
 /* colorscheme from pywal */
