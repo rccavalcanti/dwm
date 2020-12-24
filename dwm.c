5e7ea7fa72cd2806e2359740cc3ad64f3be890b0
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-12-24 20:48:00 -0300

    Disable smartgaps by default and increase gaps

--- dwm.c
+++ dwm.c
@@ -320,7 +320,7 @@ static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
 static int bh, blw = 0;      /* bar geometry */
 static int enablegaps = 1;   /* enables gaps, used by togglegaps */
-static int smartgaps = 1;    /* enables gaps, used by togglesmartgaps */
+static int smartgaps = 0;    /* enables gaps, used by togglesmartgaps */
 static int lrpad;            /* sum of left and right padding for text */
 static int (*xerrorxlib)(Display *, XErrorEvent *);
 static unsigned int numlockmask = 0;
