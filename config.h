6b8d03feb11d51b7618a3705c2799d658e7f9925
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-02-28 02:04:09 -0300

    Apply "bar height" patch

--- config.h
+++ config.h
@@ -26,6 +26,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 20;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Ubuntu Mono:size=10" };
 
 /* colorscheme from pywal or default */
